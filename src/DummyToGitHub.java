b2e274f72784bf6e1b549f2da9b7ad739d100d5b
Arla Figueiredo <arla.figueiredo@gmail.com>
2021-10-19 21:53:18 -0300

    Teste
    testando configurações

--- src/DummyToGitHub.java
+++ src/DummyToGitHub.java
@@ -3,5 +3,6 @@ public class DummyToGitHub {
 	
 	public static void main(String[] args) {
 		System.out.println("WHAT IS MY PORPOUSE? TO LOAD THIS FOLDER IN THE REPO! OHHH GOD NO");
+		System.out.println("%%%%%%%%%%%%%%%%%%%%");
 	}
 }
