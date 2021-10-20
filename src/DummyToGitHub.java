04f8b13fd657926d16e5a8b2762adf0357cdbe01
Rafael Campos <rafacampos2000@hotmail.com>
2021-10-19 21:20:51 -0300

    main na classe dummy

--- src/DummyToGitHub.java
+++ src/DummyToGitHub.java
@@ -1,4 +1,7 @@
 
 public class DummyToGitHub {
-	//IM A DUMMY CLASS JUST TO LOAD THE DIRECTORY INTO GITHUB LOOK AT ME IM DUMB!	
+	
+	public static void main(String[] args) {
+		System.out.println("WHAT IS MY PORPOUSE? TO LOAD THIS FOLDER IN THE REPO! OHHH GOD NO");
+	}
 }
