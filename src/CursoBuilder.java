0780d7edb1ef272a10bc3a5af0af382fdc46acad
Arla Figueiredo <arla.figueiredo@gmail.com>
2021-10-23 23:22:44 -0300

    Ajusta builder

--- src/CursoBuilder.java
+++ src/CursoBuilder.java
@@ -13,6 +13,14 @@ public class CursoBuilder {
 		this.disciplinas = new ArrayList<Disciplina>();
 	}
 	
+	public void setNome(String nome){
+		this.nome = nome
+	}
+
+	public void setCodigo(String codigo){
+		this.codigo = codigo;
+	}
+	
 	public void addLivro(Livro livro) throws InstantiationException, IllegalAccessException, ClassNotFoundException{
 		this.livros.add(livro);
 	}
@@ -20,9 +28,14 @@ public class CursoBuilder {
 	public void addDisciplina(Disciplina disciplina) throws InstantiationException, IllegalAccessException, ClassNotFoundException{
 		this.disciplinas.add(disciplina);
 	}
+
+	private Ementa buildEmenta(){
+		// TO DO
+	}
 	
 	public Curso build() {
-		return new Curso(codigo,nome,livros,disciplinas);
+		Ementa ementa = this.buildEmenta();
+		return new Curso(this.codigo, this.nome, this.livros, this.disciplinas);
 	}
 	
 }
